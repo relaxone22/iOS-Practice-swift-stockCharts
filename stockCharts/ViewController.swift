6edd732fa22300bc7075ea89b47287b19fca2602
tonyliu <tonyliu@mitake.com.tw>
2018-04-16 10:37:30 +0800

    Initial Commit

---
+++ stockCharts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stockCharts
+//
+//  Created by tonyliu on 2018/4/16.
+//  Copyright © 2018 mitake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
